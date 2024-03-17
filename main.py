6c20d3ce0d0f47734655bf13cab8a95bfbed33e6
Nicholas Wise <nicholasmwise@gmail.com>
2024-03-17 18:03:04 -0500

    Refactored game loop added empty editor file finished first grip implementation

--- main.py
+++ main.py
@@ -1,38 +1,58 @@
+import sys
+
 import pygame
 
-pygame.init()
+class Game:
+    def __init__(self):
+        pygame.init()
+        pygame.display.set_caption("tower defense game")
 
-SCREEN_WIDTH = pygame.display.get_desktop_sizes()[0][0]
-SCREEN_HEIGHT = pygame.display.get_desktop_sizes()[0][1]
+        self.SCREEN_WIDTH = pygame.display.get_desktop_sizes()[0][0]
+        self.SCREEN_HEIGHT = pygame.display.get_desktop_sizes()[0][1]
 
-# screen is now a "Surface" as that is the return type from setting the display mode
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), pygame.FULLSCREEN)
+        self.bg_border = 25
+        self.bg_color = (25, 25, 25)
+        self.game_bg_rect_width = self.SCREEN_WIDTH - (self.bg_border * 2)
+        self.game_bg_rect_height = self.SCREEN_HEIGHT - (self.bg_border * 2)
+        self.game_bg_rect = (self.bg_border, self.bg_border, self.game_bg_rect_width, self.game_bg_rect_height)
 
-bg_border = 25
-bg_color = (25,25,25)
-game_bg_rect_width = SCREEN_WIDTH - (bg_border*2)
-game_bg_rect_height = SCREEN_HEIGHT - (bg_border*2)
-game_bg_rect = (bg_border, bg_border, game_bg_rect_width, game_bg_rect_height)
+        self.bg_grid_width_count = 35
+        self.bg_grid_height_count = 65
+        self.bg_grid_color = (50, 50, 50)
 
-bg_grid_width_count = 5
-bg_grid_height_count = 5
-bg_grid_color = (50,50,50)
+        # screen is now a "Surface" as that is the return type from setting the display mode
+        self.screen = pygame.display.set_mode((self.SCREEN_WIDTH, self.SCREEN_HEIGHT), pygame.FULLSCREEN)
 
-# Here is the game Loop
-run = True
-while run:
+        self.clock = pygame.time.Clock()
 
-    # Here is where we are initializing the bg and bg grid
-    pygame.draw.rect(screen, bg_color, game_bg_rect)
-    # for count in range(bg_grid_width):
-    pygame.draw.line(screen, bg_grid_color, (bg_border, (game_bg_rect_height/5 + bg_border)), ((game_bg_rect_width + bg_border), (game_bg_rect_height/5 + bg_border)))
+        # Here is where we are initializing the bg and bg grid
+        pygame.draw.rect(self.screen, self.bg_color, self.game_bg_rect)
+        for count in range(self.bg_grid_width_count - 1):
+            pygame.draw.line(self.screen, self.bg_grid_color,
+                             (self.bg_border,
+                              ((count + 1) * self.game_bg_rect_height / self.bg_grid_width_count + self.bg_border)),
+                             ((self.game_bg_rect_width + self.bg_border),
+                              ((count + 1) * self.game_bg_rect_height / self.bg_grid_width_count + self.bg_border)))
 
-    # This is the event checker for each frame
-    for event in pygame.event.get():
-        # This is where we make sure the game breaks out of the loop when the player wishes to exit
-        if event.type == pygame.QUIT:
-            run = False
+        for count in range(self.bg_grid_height_count - 1):
+            pygame.draw.line(self.screen, self.bg_grid_color,
+                             (((count + 1) * self.game_bg_rect_width / self.bg_grid_height_count + self.bg_border),
+                              self.bg_border),
+                             (((count + 1) * self.game_bg_rect_width / self.bg_grid_height_count + self.bg_border),
+                              self.SCREEN_HEIGHT - self.bg_border))
 
-    pygame.display.update()
+    def run(self):
 
-pygame.quit()
+        while True:
+            # This is the event checker for each frame
+            for event in pygame.event.get():
+                # This is where we make sure the game breaks out of the loop when the player wishes to exit
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+            pygame.display.update()
+            self.clock.tick(60)
+
+
+Game().run()
