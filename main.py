08a39d890a257481650163d3f273645d7c3df3c6
nicho <nicholasmwise@gmail.com>
2024-03-22 17:20:41 -0500

    Refactored the tower functionality into it's own class, and made sure existing functionality still worked with the change

--- main.py
+++ main.py
@@ -1,18 +1,14 @@
 import sys
 
 import pygame
-from pygame.locals import *
 
-# load art for test turret
-tower_img = pygame.image.load('art/Tower.png')
-tower_pos = [226, 222]
+from scripts import tower
 
-valid_target_gizmo = pygame.image.load('art/valid_target_gizmo.png')
-# valid_target_gizmo.set_alpha(50)
-no_target_gizmo = pygame.image.load('art/no_target_gizmo.png')
-no_target_gizmo.set_alpha(50)
-no_target_gizmo_mask = pygame.mask.from_surface(no_target_gizmo)
-
+# Init our tower for testing
+# NOTE: I think we will want to init these at runtime assuming we want to place towers
+#       during the game
+tower1 = tower.Tower()
+tower1.set_tower_position([226, 222])
 
 # monster details
 monster_img = pygame.image.load('art/centipede.png')
@@ -31,10 +27,10 @@ monster_sprite = pygame.sprite.Sprite()
 monster_sprite.image = monster_img
 monster_sprite.radius = (monster_img.get_width() / 2)
 
-tower_image_pos = [int(tower_pos[0] - (tower_img.get_width() / 2)),
-                   int(tower_pos[1] - tower_img.get_height() / 2)]
-target_radius_pos = [int(tower_pos[0] - (no_target_gizmo.get_width() / 2)),
-                     int(tower_pos[1] - no_target_gizmo.get_height() / 2)]
+tower_image_pos = [int(tower1.pos[0] - (tower1.tower_img.get_width() / 2)),
+                   int(tower1.pos[1] - tower1.tower_img.get_height() / 2)]
+target_radius_pos = [int(tower1.pos[0] - (tower1.no_target_gizmo.get_width() / 2)),
+                     int(tower1.pos[1] - tower1.no_target_gizmo.get_height() / 2)]
 
 class Game:
     def __init__(self):
@@ -59,7 +55,6 @@ class Game:
 
         self.clock = pygame.time.Clock()
 
-
     def draw_grid(self):
         # Here is where we are initializing the bg and bg grid
         pygame.draw.rect(self.screen, self.bg_color, self.game_bg_rect)
@@ -83,14 +78,17 @@ class Game:
 
             # Here is where we update the position of the monster
             self.screen.blit(monster_img, monster_pos)
-            self.screen.blit(tower_img, tower_image_pos)
+            self.screen.blit(tower1.tower_img, tower_image_pos)
             monster_pos[0] += (monster_movement[1] - monster_movement[0]) * monster_move_speed
 
             # Here is where we check if the monster is in range of the turret
-            if no_target_gizmo_mask.overlap(monster_mask, (monster_pos[0] - target_radius_pos[0], monster_pos[1] - target_radius_pos[1])):
-                self.screen.blit(valid_target_gizmo, target_radius_pos)
+            if tower1.range_mask.overlap(monster_mask, (monster_pos[0] - target_radius_pos[0], monster_pos[1] - target_radius_pos[1])):
+                # self.screen.blit(valid_target_gizmo, target_radius_pos)
+                print(F"Monster in range of {tower1.tower_img}")
+
             else:
-                self.screen.blit(no_target_gizmo, target_radius_pos)
+                # self.screen.blit(no_target_gizmo, target_radius_pos)
+                pass
 
             # This is the event checker for each frame
             for event in pygame.event.get():
@@ -99,8 +97,6 @@ class Game:
                     pygame.quit()
                     sys.exit()
                 if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_UP:
-                        show_masks = not show_masks
                     if event.key == pygame.K_LEFT:
                         monster_movement[0] = True
                     if event.key == pygame.K_RIGHT:
--- scripts/tower.py
+++ scripts/tower.py
@@ -3,7 +3,21 @@ import sys
 import pygame
 
 
-def tower():
+class Tower:
+    tower_img = pygame.image.load('art/Tower.png')
+    valid_target_gizmo = pygame.image.load('art/valid_target_gizmo.png')
+    no_target_gizmo = pygame.image.load('art/no_target_gizmo.png')
+    tower_mask = pygame.mask.from_surface(tower_img)
+    range_mask = pygame.mask.from_surface(no_target_gizmo)
     attack_range = 300
     damage = 10
     attack_speed = 2
+    pos = [0, 0]
+
+    def __init__(self):
+        pygame.init()
+
+    def set_tower_position(self, pos):
+        self.pos = pos
+
+
